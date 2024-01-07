d5bfee478808859902a5a4c02b7ac4753bde9c5c
harsh9711 <87559341+harsh9711@users.noreply.github.com>
2024-01-07 08:43:22 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const TelegramBot = require("node-telegram-bot-api");
+const axios = require("axios"); 
+
+const token = "6736543750:AAG2VDRjMP_5Sesvx5E7iYS_WOzCz5H4Q2k";
+const bot = new TelegramBot(token, { polling: true });
+
+bot.on("message", async (msg) => {
+  const chatId = msg.chat.id;
+  const userInput = msg.text;
+  
+  try {
+    const response = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${userInput}&appid=049098873d6c48b04c84bd92026f8116`
+    );
+
+    const data = response.data;
+    
+    // Check if 'weather' array exists in the response
+    if (data.weather && data.weather.length > 0) {
+      const weather = data.weather[0].description;
+      const temperature = data.main.temp - 273.15;
+      const city = data.name;
+      const humidity = data.main.humidity;
+      const pressure = data.main.pressure;
+      const windSpeed = data.wind.speed;
+
+      const message = `The weather in ${city} is ${weather} with a temperature of ${temperature.toFixed(2)}°C. The humidity is ${humidity}%, the pressure is ${pressure}hPa, and the wind speed is ${windSpeed}m/s.`;
+
+      bot.sendMessage(chatId, message);
+    } else {
+      bot.sendMessage(chatId, "City doesn't exist.");
+    }
+  } catch (error) {
+    console.error("Error fetching weather data:", error);
+    bot.sendMessage(chatId, "Error fetching weather data.");
+  }
+});
